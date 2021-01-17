2d66f973276ab5c1ab0d8d50201f30f5d3020fb9
mario3316 <51048267+mario3316@users.noreply.github.com>
2021-01-17 18:43:33 +0900

    20210117

--- Programmers/Lv1/3진법뒤집기.py
+++ Programmers/Lv1/3진법뒤집기.py
@@ -1,3 +1,25 @@
+def ten_to_three(n):
+    num = []
+    
+    while True:
+        if n < 3:
+            num.append(n)
+            break
+        else:
+            num.append(n%3)
+            n = n//3
+    
+    return num
+        
+
 def solution(n):
     answer = 0
+    
+    num = ten_to_three(n)
+    num.reverse()
+
+    for i in range(len(num)):
+        answer += num[i] * (3**i)
+    
+    
     return answer
