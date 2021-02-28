08945f08863f21dccb5aa7167de18309b9c3bfac
mario3316 <51048267+mario3316@users.noreply.github.com>
2021-02-28 21:58:49 +0900

    20210228

--- BOJ/BOJ16234.java
+++ BOJ/BOJ16234.java
@@ -10,14 +10,14 @@ public class BOJ16234 {
 
 	static int N, L, R;
 	static int[][] MAP;
+	static LinkedList<LinkedList<Point>> Union;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 	static boolean[][] visited;
 	static int answer = 0;
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(
-				new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
@@ -33,15 +33,34 @@ public class BOJ16234 {
 				MAP[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+
+		while (true) {
+			visited = new boolean[N][N];
+			Union = new LinkedList<>();
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					BFS(i, j);
+				}
+			}
+
+			if (Union.size() == 0) {
+				System.out.println(answer);
+				break;
+			} else {
+				update();
+			}
+
+		}
+
 	}
 
 	static void BFS(int r, int c) {
-		int sum = 0, cnt = 0;
+		LinkedList<Point> list = new LinkedList<>();
 		Queue<Point> Q = new LinkedList<>();
 		Q.offer(new Point(r, c));
 		visited[r][c] = true;
-		sum += MAP[r][c];
-		cnt++;
+		list.add(new Point(r, c));
 
 		while (!Q.isEmpty()) {
 			Point current = Q.poll();
@@ -50,27 +69,32 @@ public class BOJ16234 {
 				int nr = current.x + dr[i];
 				int nc = current.y + dc[i];
 
-				if (isIn(nr, nc) && !visited[nr][nc]
-						&& isOpen(current.x, current.y, nr, nc)) {
+				if (isIn(nr, nc) && !visited[nr][nc] && isOpen(current.x, current.y, nr, nc)) {
 					Q.offer(new Point(nr, nc));
 					visited[nr][nc] = true;
-					sum += MAP[nr][nc];
-					cnt++;
+					list.add(new Point(nr, nc));
 				}
 			}
 		}
 
-		if (cnt > 1) {
-			answer++;
-			int avg = sum / cnt;
+		if (list.size() > 1) {
+			Union.add(list);
+		}
+	}
 
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (visited[i][j])
-						MAP[i][j] = avg;
-				}
+	static void update() {
+		for (LinkedList<Point> l : Union) {
+			int sum = 0;
+			for (Point p : l) {
+				sum += MAP[p.x][p.y];
+			}
+			int avg = sum / l.size();
+			for (Point p : l) {
+				MAP[p.x][p.y] = avg;
 			}
 		}
+
+		answer++;
 	}
 
 	static boolean isOpen(int r, int c, int nr, int nc) {
