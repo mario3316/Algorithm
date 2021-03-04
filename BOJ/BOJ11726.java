1ffac6d30f7786236ad4e7aa580ee2da8a5b697f
mario3316 <51048267+mario3316@users.noreply.github.com>
2021-03-04 23:51:21 +0900

    20210304

--- BOJ/BOJ11726.java
+++ BOJ/BOJ11726.java
@@ -7,7 +7,7 @@ public class BOJ11726 {
         if (n > 0) d[1] = 1;
         for (int i=2; i<=n; i++) {
             d[i] = d[i-2] + d[i-1];
-            d[i] %= 10007; // 이 부분도 마찬가지
+            d[i] %= 10007;
         }
         System.out.println(d[n]);
         
