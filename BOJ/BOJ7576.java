dc2b47ec802cc7e2d36a27f1fb661f35c5241265
mario3316 <wlshgktm@gmail.com>
2021-03-13 23:47:59 +0900

    20210313

--- BOJ/BOJ7576.java
+++ BOJ/BOJ7576.java
@@ -9,15 +9,12 @@ public class BOJ7576 {
 
 	static int N, M;
 	static int[][] MAP;
-	static boolean[][] visited;
+	static int[][] visited;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
-	static int answer = 0;
+	static Queue<int[]> Q = new LinkedList<>();
 
 	public static void main(String[] args) throws IOException {
-		/*
-		 * <로직> 1. 이중 for문 돌면서 익은 토마토들 주위를 익힘 2. 이중 for문 중 안익은 토마토가 없으면 끝
-		 */
 		BufferedReader br = new BufferedReader(
 				new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,110 +22,57 @@ public class BOJ7576 {
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
 		MAP = new int[N][M];
+		visited = new int[N][M];
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				MAP[i][j] = Integer.parseInt(st.nextToken());
+				if (MAP[i][j] == 1)
+					Q.offer(new int[] { i, j });
 			}
 		}
 
 		BFS();
-
-		System.out.println("!!!!!");
-		if (answer != -1)
-			solve();
-
-		System.out.println(answer);
-	}
-
-	static boolean validate() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (MAP[i][j] == 0) {
-					BFS();
-				}
-			}
-		}
-
-	}
-
-	static boolean BFS() {
-		Queue<int[]> Q = new LinkedList<>();
-		visited = new boolean[N][M];
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (MAP[i][j] == 0) {
-					Q.offer(new int[] { i, j });
-					visited[i][j] = true;
-
-					while (!Q.isEmpty()) {
-						int[] current = Q.poll();
-
-						if (MAP[current[0]][current[1]] == 1)
-							return true;
-
-						for (int d = 0; d < 4; d++) {
-							int nr = current[0] + dr[d];
-							int nc = current[1] + dc[d];
-
-							if (isIn(nr, nc) && !visited[nr][nc]
-									&& MAP[i][j] != -1) {
-								Q.offer(new int[] { nr, nc });
-								visited[nr][nc] = true;
-							}
-						}
-					}
-
-					return false;
-				}
-			}
-		}
-	}
-
-	static void solve() {
-		while (true) {
-			boolean flag = false;
-			visited = new boolean[N][M];
-
+		if (!checkMap()) {
+			System.out.println("-1");
+		} else {
+			int answer = 0;
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < M; j++) {
-					if (MAP[i][j] == 1) {
-						rot(i, j);
-					} else if (MAP[i][j] == 0) {
-						flag = true;
-					}
+					answer = Math.max(answer, visited[i][j]);
 				}
 			}
 
-			if (!flag) {
-				break;
-			} else {
-				update();
-				answer++;
+			System.out.println(answer);
+		}
+	}
+
+	static void BFS() {
+		while (!Q.isEmpty()) {
+			int[] current = Q.poll();
+
+			for (int i = 0; i < 4; i++) {
+				int nr = current[0] + dr[i];
+				int nc = current[1] + dc[i];
+
+				if (isIn(nr, nc) && visited[nr][nc] == 0 && MAP[nr][nc] == 0) {
+					Q.offer(new int[] { nr, nc });
+					visited[nr][nc] = visited[current[0]][current[1]] + 1;
+				}
 			}
 		}
 	}
 
-	static void rot(int r, int c) {
-		for (int i = 0; i < 4; i++) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-
-			if (isIn(nr, nc) && MAP[nr][nc] == 0) {
-				visited[nr][nc] = true;
-			}
-		}
-	}
-
-	static void update() {
+	static boolean checkMap() {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (visited[i][j])
-					MAP[i][j] = 1;
+				if (MAP[i][j] == 0 && visited[i][j] == 0)
+					return false;
 			}
 		}
+
+		return true;
 	}
 
 	static boolean isIn(int r, int c) {
