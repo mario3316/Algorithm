541adbc7673dfab7ba890acfc9028af8bd327d15
mario3316 <51048267+mario3316@users.noreply.github.com>
2021-03-30 23:29:04 +0900

    20210330

---
+++ BOJ/BOJ1450.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ1450 {
+
+    static int N, C;
+    static int[] weight;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        weight = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            weight[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(weight);
+
+    }
+
+}
