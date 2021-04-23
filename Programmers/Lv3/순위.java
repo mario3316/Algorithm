32555e0f03d916ea289ea6a8fcd2aabe5d9e2b07
mario3316 <wlshgktm@gmail.com>
2021-04-23 14:29:00 +0900

    20210422

--- Programmers/Lv3/순위.java
+++ Programmers/Lv3/순위.java
@@ -1,16 +1,22 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class 순위 {
 
     static ArrayList<Integer>[] edges;
-    static int[] count;
+    static int[][] result;
+
+    public static void main(String[] args) {
+        순위 s = new 순위();
+        s.solution(5, new int[][] { { 4, 3 }, { 4, 2 }, { 3, 2 }, { 1, 2 }, { 2, 5 } });
+    }
 
     public int solution(int n, int[][] results) {
         int answer = 0;
         edges = new ArrayList[n + 1];
-        count = new int[n + 1];
+        result = new int[n + 1][n + 1];
 
         for (int i = 1; i <= n; i++) {
             edges[i] = new ArrayList<>();
@@ -25,8 +31,16 @@ public class 순위 {
         }
 
         for (int i = 1; i <= n; i++) {
-            if (count[i] >= n - 1)
+            int count = 0;
+            for (int j = 1; j <= n; j++) {
+                if (result[i][j] != 0)
+                    count++;
+            }
+
+            if (count == n - 1)
                 answer++;
+
+            System.out.println(Arrays.toString(result[i]));
         }
 
         return answer;
@@ -45,13 +59,9 @@ public class 순위 {
                 if (!visited[next]) {
                     Q.add(next);
                     visited[next] = true;
+                    result[next][start] = -1;
+                    result[start][next] = 1;
                 }
-            }
-        }
-
-        for (int i = 1; i <= n; i++) {
-            if (visited[i]) {
-                count[i]++;
             }
         }
     }
